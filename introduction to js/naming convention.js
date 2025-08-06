bca769ada5781866b8b1d73890af98e01a99a929
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-06 16:11:06 +0530

    naming convention added

---
+++ introduction to js/naming convention.js
@@ -0,0 +1,11 @@
+// Naming convention for variiables :
+//      - starts with a letter , $ , _.
+//      - hyphens and spaces are not allowed.
+//      - case sensitive , so will use camel casing.
+//      - don't naming variable with javascript keyword.
+
+// For Example :
+
+        var fatherName ;
+        var $number ;
+        var _namingVariable ;
