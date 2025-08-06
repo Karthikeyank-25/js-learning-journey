73e329625746e986b18ed37c6533de9ae7a96f84
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-06 15:35:49 +0530

    alert file updated

--- introduction to js/Alerts.js
+++ introduction to js/Alerts.js
@@ -1,1 +1,9 @@
+// Alert Popup :
+//      This popup will display whatever inbetween of the paranthesis.
+// Alert syntax :
 
+    alert("alert message");
+
+// For Example :
+
+    alert("This is Karthi !!");
