2b88f02b06d02f92ee28440747ec83107fc691f0
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-06 16:23:58 +0530

    string concatenation added

---
+++ introduction to js/string concatenation.js
@@ -0,0 +1,8 @@
+// String Concatenation :
+//          Adding two or more separate string into single string by using "+".
+
+// Syntax :
+            "string-1" + "string-2"
+
+// For Example :
+            alert("My name is" + "Karthikeyan");
