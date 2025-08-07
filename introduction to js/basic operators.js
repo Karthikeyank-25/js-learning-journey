7bba91f04e16a9f5b83d0af3aaabb29cc1d82d54
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 11:13:42 +0530

    Basic operators added

---
+++ introduction to js/basic operators.js
@@ -0,0 +1,16 @@
+// Basic Operators :
+//          The operators are used to perform operations of Addition , Subtraction , Multiplication , Division.
+//          The signs of the operators [ + , - , * , / ]
+
+// Syntax :
+            c = a + b; // (for addition)
+            c = a - b; // (for subtraction)
+            c = a * b; // (for multiplication)
+            c = a / b; // (for division)
+
+// For Example :
+            var a = 908;
+            var b = 567;
+            var c = 787;
+            var priceAmount ;
+            priceAmount = (a + b)*2 / c;
