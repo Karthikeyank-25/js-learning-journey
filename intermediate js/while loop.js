8454578133a5d28f3f89c8e7f1b5c98d8d4323c6
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 23:34:27 +0530

    whille loop added

---
+++ intermediate js/while loop.js
@@ -0,0 +1,16 @@
+// While Loop :
+//          It is the loop , the loop will continue to runs the function until the condition given inn the loop is get failed.
+
+// Syntax :
+            while(condition){
+
+            }
+
+// For Example :
+            var a = [];
+            let n = 1;
+            while(n <= 234){
+                a.push(n);
+                console.log(a)
+                n++;
+            }   
