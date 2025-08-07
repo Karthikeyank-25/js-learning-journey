57bc61f00a4f537d34b53c555ff85c00e2447451
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 09:13:59 +0530

    String length retriving file added

---
+++ introduction to js/string length retriving.js
@@ -0,0 +1,10 @@
+// String length retriving :
+//          It is used to calculate numberr of characters in the string.
+
+// Syntax :
+            var name = "String";
+            name.length;
+
+// For Example :
+            var schoolName = "Kamalammal"
+            schoolName.length;
