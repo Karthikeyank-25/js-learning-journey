8eda6f930f50344b883b84a8bca7a2006045db21
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 10:52:15 +0530

    incrementing and decrementing file added

---
+++ introduction to js/increment and decrement.js
@@ -0,0 +1,16 @@
+// Increment and Decrement :
+//          It is one of the technique to incrementing (increasing value) and decrementing (decreasing value) value from the give value (number).
+
+// Syntax : (one value incrementing and decrementing)
+            a++ // (for incrementing)
+            a-- // (for decrementing)
+
+// Syntax : (multiple value incrementing and decrementing)
+            x += y; // (for incrementing)
+            x -= y; // (for decrementing)
+
+// For Example :
+            x = 23;
+            y = 72;
+            x += y; // (incrementing)
+            x -= y; // (decrementing)
