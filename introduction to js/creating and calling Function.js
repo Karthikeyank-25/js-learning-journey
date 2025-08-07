2f211ff92fc9bc8ffc607d8da68fcf9b685a1849
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 14:05:36 +0530

    creating and calling functions added

---
+++ introduction to js/creating and calling Function.js
@@ -0,0 +1,17 @@
+// Creating and calling function :
+//          Function is a named block of code that performs a specific task. You can define a function once and reuse it multiple times in your program.
+
+// Syntax :
+            function fatherName (){
+                alert("Is your's father name is Kandan.")   // for creating a function
+            }
+
+            fatherName();                                   // for calling a function
+
+// For Example :
+            function getName (){
+                var personName = prompt("What is your name ?");
+                alert("Hi ! "+ personName);                     // creating function
+            }
+
+            getName();                                          // calling function
