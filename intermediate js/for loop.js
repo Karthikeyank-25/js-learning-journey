882dc2c9a453c1ba871d7bc1ba38f1af6ebb0806
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 23:53:41 +0530

    For loop added

---
+++ intermediate js/for loop.js
@@ -0,0 +1,14 @@
+// For Loop :
+//      It is the loop , in this loop all the condition are predefined like (start,end,wise) while declering a loop.
+
+// Syntax :
+        for(i=1;i<n;i++){
+            
+        }
+
+// For Example :
+        let tickBox = [];
+        for(let i=1;i<=1000;i++){
+                tickBox.push(i);
+        }
+        console.log(tickBox);
