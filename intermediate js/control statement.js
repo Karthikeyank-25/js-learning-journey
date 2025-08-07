b61b9b76d05096e8c4619e2790f63deea02cdcdc
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 21:24:13 +0530

    Control statement added

---
+++ intermediate js/control statement.js
@@ -0,0 +1,27 @@
+// If and Else (Control Statement) :
+//          It is control flow statements used to make decisions in your code.They allow you to run different blocks of code depending on whether a condition is true or false.
+
+// Syntax :
+            if(condition){
+
+            }
+            else if(condition){
+
+            }
+            else{
+
+            }
+
+// For Example :
+            let nativePlace = 100 * Math.random();
+            
+            if(nativePlace >= 70){
+                alert("Your are a chennai boy bro!!");
+            }
+            else if(40 <= nativePlace && nativePlace < 70 ){
+                alert("Your are covai boy buddy!!");
+            }
+            else{
+                alert("Your are tamil guy friend!!");
+            }
+ 
