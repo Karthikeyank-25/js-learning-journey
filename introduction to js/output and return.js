9b6803411b63d41539f1012fe9b17ce2a08a8ca6
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 14:58:05 +0530

    Output and Return added

---
+++ introduction to js/output and return.js
@@ -0,0 +1,11 @@
+// Output and Return :
+//      - Output , We cannot use the output in another variable or anything.
+//      - But return , it makes the functions output into a usable value programmatically not only for displaying a value.
+//        it returning values and outputs from functions.
+
+// Syntax :
+        function drivingJob(start,end){
+            let a = "The trip starts from "+start+" and ends in "+end+".";  // returning functions output
+            return a;
+        }
+        drivingJob("Tiruvannamalai","Chennai");                             // calling function and returning output
