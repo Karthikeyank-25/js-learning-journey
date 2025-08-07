a05940780ff8ca9d5263f3df845202e62e11f825
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 23:02:45 +0530

    array added

---
+++ intermediate js/arrays.js
@@ -0,0 +1,22 @@
+// Arrays :
+//      -The array is also a variable,but it is special varaiable.
+//      -It is used store multiple values within that array.
+//      -It also stores different datatypes within it.
+//      -It also mentioned inbetween of square brackets.
+
+// Syntax :
+        var arrayName = [];
+
+// For Example :
+        var fruitName = ["Apple","Orange","Banana","Gova"];
+
+// Methods :
+        fruitName.includes("Banana");   // check values
+        fruitName.indexOf("Gova");      // find index value
+        fruitName.push("Mango");        // add at the end
+        fruitName.pop();                // remove from end
+        fruitName.unshift("Papaya");    // add at start
+        fruitName.shift();              // remove from start
+        fruitName.length();             // find the length
+        fruitName.slice();              // (start index value , no of values from index , replacing value for that index postion)
+        fruitName.slice();              // cut the specific values (start,end)
