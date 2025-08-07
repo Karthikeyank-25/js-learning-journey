3257755cf25e4d89b2f966ee9134c44e63910254
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 09:29:25 +0530

    Case changing in text added

---
+++ introduction to js/change casing in text.js
@@ -0,0 +1,10 @@
+// Case changing in text :
+//      It is used to change the casing of the text by using keywords.
+
+// Syntax :
+        var typeName = "String";
+        typeName.toUpperCase();
+
+// For Example :
+        var emailAddress = "karthikeyankandan25@gmail.com";
+        emailAddress.toUpperCase();
