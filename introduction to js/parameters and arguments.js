dc481a6d3eb4c8e062db756a044767150b4e7544
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-07 14:28:30 +0530

    parameters and Arguments are added

---
+++ introduction to js/parameters and arguments.js
@@ -0,0 +1,17 @@
+// Parameters and Arguments :
+//      The input that given into the function is "PARAMETERS".
+//      The value that set to that input (parameter) is known as "ARGUMENTS".
+
+// Syntax :
+        function fatherName (father){
+                alert("Is your's father name is " + father)   // Setting the parameters
+            }
+
+        fatherName("Kandan");                                 // Setting the Arguments
+
+// For Example :
+        function tailoringWork (shirts,pants){
+            alert("your's "+shirts+" shirts and your's "+pants+" pants is ready now !!!"); // Setting parameters
+        }
+
+        tailoringWork(4,2);                                                                // Setting arguments
