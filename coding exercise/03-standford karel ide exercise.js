9488a1ad46ff8618120324d860e012efd7d3d729
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-08 14:59:56 +0530

    Karel IDE exercise

---
+++ coding exercise/03-standford karel ide exercise.js
@@ -0,0 +1,37 @@
+// Standford Karrel IDE Exercise :
+// Welcome to the Stanford Karel IDE.
+// This is a free space for you to 
+// write any Karel program you want.
+ 
+function main(){
+   stepTwo();   
+}
+function stepOne(){
+   move();
+   turnLeft();
+   move();
+   putBeeper();
+   move();
+   turnLeft();
+   move();
+   turnLeft();
+   move();
+   turnLeft();
+   move();   
+}
+function stepTwo(){
+    stepOne();
+    stepOne();
+    stepOne();
+    stepOne();
+    stepOne();
+    stepOne();
+    stepOne();
+    stepOne();
+    stepOne();
+    stepOne();
+    stepOne();
+    stepOne();
+    stepOne();
+
+}
