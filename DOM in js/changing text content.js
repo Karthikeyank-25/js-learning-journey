31d1921a84d2b0749633d727c5b3ed72b7fc0df1
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-08 12:56:00 +0530

    Changing text in Html

---
+++ DOM in js/changing text content.js
@@ -0,0 +1,16 @@
+// Changing text content :
+//              It is used to edit , alter , includes texts into the content by using some properties.
+//                  - innerHTML
+//                  - textContent
+
+// innerHTML :
+//              It is used to edit content within the "H" tags.But it includes whatever present inbetween of the "H" tag.
+
+// Syntax :
+                document.firstElementChild.querySelector("li h1").innerHTML("hi!");
+
+// textContent :
+//              It is used to edit text content only within the "H" Tags.
+
+// Syntax :
+                document.firstElementChild.querySelector(".first h2").textContent("I'm Karthi");
