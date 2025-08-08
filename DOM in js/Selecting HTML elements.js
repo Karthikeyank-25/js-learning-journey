57d0cb2442bba83bbe307f38cc8bbd7a0748e7b3
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-08 12:06:01 +0530

    Selecting html elements added

---
+++ DOM in js/Selecting HTML elements.js
@@ -0,0 +1,9 @@
+// Selecting Html Elements :
+//          It is used to select html elements in the documents by differet methods.
+
+// Syntax :
+                    - document.getElementById("");            // select by ID name
+                    - document.getElementsByClassName("");    // select by Class name
+                    - document.getElementsByTagName("");      // select by Tag name
+                    - document.querySelector("");             // select by guery like (.class,#ID) & (li a h1)
+                    - document.querySelectorAll("");          // select all element present within that query
