9ed47b6b4ac66821de07f9c58b9cc3b68675068c
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-08 12:40:27 +0530

    Adding classes into the Html tags

---
+++ DOM in js/adding classes.js
@@ -0,0 +1,7 @@
+// Adding classes :
+//          It is used to add classes into the HTML Tags by adding property in the javaScript code.
+
+// Syntax :
+            document.firstElementChild.classList;                           // Check the classes in that tag
+            document.firstElementChild.classList.add("addClasses");         // Add classes into the tag
+            document.firstElementChild.classList.remove("removeClasses");   // Remove classes from that tag
