4446045c91a69bdedc6f80bf1cf14d68655159f6
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-08 11:19:47 +0530

    DOM model added

---
+++ DOM in js/DOM definition.js
@@ -0,0 +1,19 @@
+// DOM (Document Objet Model) :
+//          All the tags are documented.The tags are known as objects in the documents.
+//          All the objects found at documents have two things.
+//                  - Property
+//                  - Methods
+
+// Property :
+//          Describing the objects in the documents.It has two property.
+//                  - Getting Property  (document.car.color;)
+//                  - Setting Property  (document.car.colour = blue;)
+
+// Methods :
+//          It describes what are the functions able to do by that object.
+//                  - Calling Method
+
+// For Example :
+            document.firstElementChild.shirt.color;         // Getting Property
+            document.firstElementChild.shirt.color = red ;  // Setting Property
+            document.firstElementChild.shirt.iron();        // Calling Property
