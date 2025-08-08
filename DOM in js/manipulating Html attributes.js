e8406150e1de5efaaa2aa0aa1420746c534ddb98
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-08 13:04:42 +0530

    Manipulating html attributes

---
+++ DOM in js/manipulating Html attributes.js
@@ -0,0 +1,7 @@
+// Manipulating html attributes :
+//              It is used to manipulates the attributes of the html tags by using javaScript property.
+
+// Syntax :
+                document.firstElementChild.attributes;                              // Check the available attributes of html tag
+                document.firstElementChild.getAttribute(".first");                  // Getting the attributes of html tag
+                document.firstElementChild.setAttribute(".first,www.google.com");   // Setting the attribute value to the tag
