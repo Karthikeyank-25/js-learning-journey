5ab045dad118b2e9dc0f323aa46c42539c03d9ea
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-10 20:02:31 +0530

    Higher order function

---
+++ advance javaScript and DOM manipulation/higher order functions.js
@@ -0,0 +1,26 @@
+// Higher order functions :
+//          Higher order functions are functions that can take other functions as input.
+
+// Synatx :
+
+function higherOrder(argument1Name,argument2Name,functionName){
+    return ;
+}
+
+// For Example :
+
+
+function oneDay(food,living){
+    return food*1+living*1;
+}
+function twoDay(food,living){
+    return food*2+living*2;
+}
+function travelTrip(food,living,expences){
+    function expences(food,living){
+        return expences;
+    }
+}
+
+
+
